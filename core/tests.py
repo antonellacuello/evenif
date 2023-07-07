426343f53635604b990f9636d4069640c8681002
antonellacuello <122099426+antonellacuello@users.noreply.github.com>
2023-07-07 14:18:52 +0000

    test

--- core/tests.py
+++ core/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 class TestHome(TestCase):
     def setUp(self):
-        self.response == self.client.get('/')
+        self.response = self.client.get('/')
 
     def test_home(self):
         self.assertEqual(self.response.status_code, 200)
