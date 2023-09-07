95bcfde6f8b00d7712b29cfe2ff97d2772a0b834
antonellacuello <122099426+antonellacuello@users.noreply.github.com>
2023-09-07 02:40:33 +0000

    commit

--- eventif/urls.py
+++ eventif/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import home
-from subscriptions.views import subscribe 
+from subscriptions.views import subscribe
 
 urlpatterns = [
     path('', home),
     path('inscricao/', subscribe),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
---
+++ subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from subscriptions.forms import SubscriptionForm
+from django.core import mail
+
+class MailTest(TestCase):
+    def setUp(self):
+        data = dict(name='Antonella Cuello', cpf='12345678901', email='antonella.cuello@aluno.riogrande.ifrs.edu.br', phone='53981451460')
+        self.response = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+        
+    def test_subscription_email_subject(self):
+        expect = "Confirmação de inscrição"
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_sender(self):
+        expect = 'contato@eventif.com.br'
+        self.assertEqual(expect, self.email.from_email)
+    
+    def test_subscription_email_to(self):
+        expect = ['contato@eventif.com.br', 'antonella.cuello@aluno.riogrande.ifrs.edu.br']  # Atualize os endereços de e-mail aqui
+        self.assertEqual(expect, self.email.to)
+    
+    def test_subscription_email_body(self):
+        contents = ['Antonella Cuello','12345678901', 'antonella.cuello@aluno.riogrande.ifrs.edu.br', '53981451460']
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
