d1a7c3dfcaebdccdca0a86277563e1126b2d4501
antonellacuello <122099426+antonellacuello@users.noreply.github.com>
2023-09-15 01:48:48 +0000

    criação do formulário de contato

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
-# Create your views here.
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/contact_form.html', {'form': form})
