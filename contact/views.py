973d9ff4db28fc8f74619663dd643c22feba3ecd
antonellacuello <122099426+antonellacuello@users.noreply.github.com>
2023-09-16 23:51:22 +0000

    correção do metodo get

--- contact/views.py
+++ contact/views.py
@@ -5,21 +5,24 @@ def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)  
 
-    if not form.is_valid():
+        if not form.is_valid():
+            return render(request, 'contact_form.html', {'form': form})
+
+        
+        _send_mail(
+            form.cleaned_data['subject'],
+            settings.DEFAULT_FROM_EMAIL,
+            form.cleaned_data['email'],
+            'contact_email.txt',
+            form.cleaned_data
+        )
+
+        messages.success(request, 'Mensagem enviada com sucesso!')
+        return HttpResponseRedirect('/contact')
+    else:
+        form = ContactForm()
         return render(request, 'contact_form.html', {'form': form})
-
-    
-    _send_mail(
-        form.cleaned_data['subject'],
-        settings.DEFAULT_FROM_EMAIL,
-        form.cleaned_data['email'],
-        'contact_email.txt',
-        form.cleaned_data
-    )
-
-    messages.success(request, 'Mensagem enviada com sucesso!')
-    return HttpResponseRedirect('/contact')
-
+        
 def _send_mail(subject, from_, to, template_name, context):
     body = render_to_string(template_name, context)
     mail.send_mail(subject, body, from_, [from_, to])
