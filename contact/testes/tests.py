c919c9fcfe9eb3ae67942716b1ecf21aa70bea0c
antonellacuello <122099426+antonellacuello@users.noreply.github.com>
2023-09-17 00:26:59 +0000

    testes

---
+++ contact/testes/tests.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.core import mail
+from contact.forms import ContactForm
+
+class ContactViewTestes(TestCase):
+    def test_get_contact_page(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact_form.html')
+
+    def test_valid_post_contact_form(self):
+        form_data = {
+            'name': 'Antonella Cuello',
+            'phone': '53981451460',
+            'email': 'antonella.cuello@aluno.riogrande.ifrs.edu.br',
+            'message': 'HAHAHAHAHAHHAHAHAHAHAHA'
+        }
+        response = self.client.post(reverse('contact'), data=form_data)
+        self.assertEqual(response.status_code, 302)  
+        self.assertEqual(len(mail.outbox), 1)  
+
+    def test_invalid_post_contact_form(self):
+        form_data = {}  
+        response = self.client.post(reverse('contact'), data=form_data)
+        self.assertEqual(response.status_code, 200)  
+        self.assertFormError(response, 'form', 'name', 'This field is required.')
+        self.assertFormError(response, 'form', 'email', 'This field is required.')
+        self.assertFormError(response, 'form', 'message', 'This field is required.')
+
+    def test_send_email(self):
+        mail.send_mail(
+            'Mensagem de Contato',
+            'Mensagem Teste',
+            'antonella.cuello@aluno.riogrande.ifrs.edu.br',
+            ['contato@eventif.com.br']
+        )
+        self.assertEqual(len(mail.outbox), 1)  
+        sent_email = mail.outbox[0]
+        self.assertEqual(sent_email.subject, 'Mensagem de Contato')
+        self.assertEqual(sent_email.from_email, 'antonella.cuello@aluno.riogrande.ifrs.edu.br')
+        self.assertEqual(sent_email.to, ['contato@eventif.com.br'])
