0044bac6c39f2ef62e2fa1938f1b60843cbf111f
antonellacuello <122099426+antonellacuello@users.noreply.github.com>
2023-12-01 14:43:34 +0000

    atualizações do subscription

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -1,7 +1,22 @@
-from django import forms
+from django import forms 
+from subscriptions.models import Subscription
+from subscriptions.validators import validate_cpf
+from django.core.exceptions import ValidationError
 
-class SubscriptionForm(forms.Form):
-    name = forms.CharField(label="Nome")
-    cpf = forms.CharField(label="CPF")
-    email = forms.EmailField()
-    phone = forms.CharField(label="Telefone")
+class SubscriptionForm(forms.ModelForm):
+
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
+
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        words = [w.capitalize() for w in name.split()]
+        return ' '.join(words)
+
+    def clean(self):
+        self.cleaned_data = super().clean()
+        if (not self.cleaned_data['email'] and \
+            not self.cleaned_data['phone']):
+            raise ValidationError('Informe seu email ou telefone')
