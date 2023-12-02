be0c13f662cddbc681bb8d02582899a76ca6e235
antonellacuello <122099426+antonellacuello@users.noreply.github.com>
2023-12-02 21:18:08 +0000

    model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=12)
+    email = models.EmailField()
+    message = models.TextField()
+    response = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
