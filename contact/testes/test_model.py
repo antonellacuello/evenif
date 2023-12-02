8c77e3886da96e8fb6359fadf27a0d7dfc216494
antonellacuello <122099426+antonellacuello@users.noreply.github.com>
2023-12-02 23:38:14 +0000

    ok

--- contact/testes/test_model.py
+++ contact/testes/test_model.py
@@ -6,15 +6,15 @@ from contact.models import Contact
 class ContactModelTest(TestCase):
     def setUp(self):
         self.contact = Contact.objects.create(
-            name='Exemplo de Contato',
-            email='exemplo@email.com',
-            message='Mensagem de exemplo',
+            name='Contato',
+            email='antonella@email.com',
+            message='Mensagem',
         )
 
     def test_contact_creation(self):
-        self.assertEqual(self.contact.name, 'Exemplo de Contato')
-        self.assertEqual(self.contact.email, 'exemplo@email.com')
-        self.assertEqual(self.contact.message, 'Mensagem de exemplo')
+        self.assertEqual(self.contact.name, 'Contato')
+        self.assertEqual(self.contact.email, 'antonella@email.com')
+        self.assertEqual(self.contact.message, 'Mensagem')
         self.assertIsNotNone(self.contact.created_at)
 
     def test_response_field(self):
